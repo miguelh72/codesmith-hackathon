3c53a1f7d4042326177d69e73743596741d981f5
Miguel Hernandez <miguelh72@outlook.com>
2021-09-23 17:43:11 -0400

    fixed scrapper to only keep urls with codesmith or zoom
    Signed-off-by: Miguel Hernandez <miguelh72@outlook.com>

--- public/scraper.js
+++ public/scraper.js
@@ -4,27 +4,30 @@ const interval = 2000;
 
 chrome.storage.sync.get(['name'], (result) => {
   const name = result.name;
+  name[0] = name[0].trim();
+  name[1] = name[1].trim();
 
   setInterval(() => {
-    console.log(`Injected with name ${name}`); // TODO RM
-
     // scrape for div's with class c-message_kit__gutter__right
     const posts = [...document.querySelectorAll('div.c-message_kit__gutter__right')];
+
     for (let i = posts.length - 1; i >= 0; i--) {
       const post = posts[i];
 
       // find first post to mention the user's name
-      const nameMatcher = new RegExp(`${name[0]}[^<]*${name[1]}[^<]*(<a[^>]*href="(.*?)"[^>]*>)`, 'i');
+      const nameMatcher = new RegExp(`${name[0]}[^<]*${name[1]}(.*?<br.*?>.*?)*?(<a[^>]*href="(.*?)"[^>]*>)`, 'i');
+      //const nameMatcher = new RegExp(`${name[0]}[^<]*${name[1]}.*?(<a[^>]*href="(.*?)"[^>]*>)`, 'i');
+      // const nameMatcher = new RegExp(`${name[0]}[^<]*${name[1]}[^<]*(<a[^>]*href="(.*?)"[^>]*>)`, 'i');
       const matchResult = post.innerHTML.match(nameMatcher); // index 2 is href
 
-      if (matchResult && typeof matchResult[2] === 'string') {
-        const link = matchResult[2];
+      if (matchResult && typeof matchResult[3] === 'string') {
+        const link = matchResult[3];
         // TODO add regex to confirm link is valid
-        if (!link.match(/https*:\/\//i)) continue;
+        if (!link.match(/https*:\/\/.*(codesmith|zoom)/i)) continue;
 
         // update local storage with latest link
         chrome.storage.sync.set({ lastPPLink: link }, () => {
-          console.log('Link is set to ' + link + 'in local storage'); // TODO remove
+          console.log('\nWIZARD: Link is set to "' + link + '" in local storage\n'); // TODO remove
         });
 
         break;
