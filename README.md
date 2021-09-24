20ab31837c0d52cff96440ef6f6c43181e079761
Miguel Hernandez <miguelh72@outlook.com>
2021-09-24 09:29:21 -0400

    Added more items to dev roadmap. Cleaned up scraper code
    Signed-off-by: Miguel Hernandez <miguelh72@outlook.com>

--- README.md
+++ README.md
@@ -26,4 +26,11 @@ Please see [whiteboard](https://excalidraw.com/#room=cc4452a38e19f8fc55c1,GjVzMa
     * [x] page should check if we are in #general room and parse through any matches of the user's name and associated link
 * [x] Have an options page where user can change their name
     * [] optionally add list of users to specifically look for mentions, default is to search through all mentions
-* Notify user if last URL is older than 2 days
+* [ ] Add dropdown with currently active cohorts for user to choose when registering.
+* [ ] Notify user if last URL is older than 2 days, ask them to open Slack again
+* [ ] Refactor units of functionality into testable files. Create tests of all features and views. Mock Google chrome storage API.
+* [ ] Integrate Google calendar API to fetch Codesmith assigned activities with zoom links.
+    * [ ] Add push notification system based on where you need to be next.
+* [ ] Use OAuth user authorization, launch a simple backend to collect user statistics.
+* [ ] Integrate with Codesmith backend to provide staff with ability to update currently active cohort Slack organization links.
+
--- public/scraper.js
+++ public/scraper.js
@@ -1,11 +1,9 @@
 /* global chrome */
 
-const interval = 2000;
+const SCRAPE_TIMEOUT = 2000; // ms
 
 chrome.storage.sync.get(['name'], (result) => {
-  const name = result.name;
-  name[0] = name[0].trim();
-  name[1] = name[1].trim();
+  const name = result.name.map(str => str.trim());
 
   setInterval(() => {
     // scrape for div's with class c-message_kit__gutter__right
@@ -16,24 +14,22 @@ chrome.storage.sync.get(['name'], (result) => {
 
       // find first post to mention the user's name
       const nameMatcher = new RegExp(`${name[0]}[^<]*${name[1]}(.*?<br.*?>.*?)*?(<a[^>]*href="(.*?)"[^>]*>)`, 'i');
-      //const nameMatcher = new RegExp(`${name[0]}[^<]*${name[1]}.*?(<a[^>]*href="(.*?)"[^>]*>)`, 'i');
-      // const nameMatcher = new RegExp(`${name[0]}[^<]*${name[1]}[^<]*(<a[^>]*href="(.*?)"[^>]*>)`, 'i');
-      const matchResult = post.innerHTML.match(nameMatcher); // index 2 is href
+      const matchResult = post.innerHTML.match(nameMatcher); // matching group 2 is href
 
       if (matchResult && typeof matchResult[3] === 'string') {
         const link = matchResult[3];
-        // TODO add regex to confirm link is valid
+        // confirm link is valid
         if (!link.match(/https*:\/\/.*(codesmith|zoom)/i)) continue;
 
         // update local storage with latest link
         chrome.storage.sync.set({ lastPPLink: link }, () => {
-          console.log('\nWIZARD: Link is set to "' + link + '" in local storage\n'); // TODO remove
+          console.log('\nCODESMITH WIZARD: Link is set to "' + link + '" in local storage\n'); // TODO remove
         });
 
         break;
       }
     }
-  }, interval);
+  }, SCRAPE_TIMEOUT);
 });
 
 
